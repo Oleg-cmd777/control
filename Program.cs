a487ad5e7f07e197946087904f9868a3fc9776e5
Oleg <oleg@example.com>
2022-09-18 14:01:43 +0300

    code3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+﻿string[] array1 = new string[] {"1234", "1567", "-2", "computer scines"};
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
